a2a0e5fb5eef93f1b4eb48c717769d875a768483
Johannes Egger <j.egger@posteo.at>
2022-01-12 15:00:27 +0100

    Clean up

--- Htlvb.Audio/Audio.cs
+++ Htlvb.Audio/Audio.cs
@@ -5,7 +5,7 @@ public class Audio
     public Audio(float[] samples, int samplesPerSecond, int channels)
     {
         Samples = samples ?? throw new ArgumentNullException(nameof(samples));
-        SamplesPerSecond = samplesPerSecond > 0 ? samplesPerSecond : throw new ArgumentException("Sample rate must be positive.");;
+        SamplesPerSecond = samplesPerSecond > 0 ? samplesPerSecond : throw new ArgumentException("Sample rate must be positive.");
         Channels = channels > 0 ? channels : throw new ArgumentException("Number of channels must be positive.");
     }
 
